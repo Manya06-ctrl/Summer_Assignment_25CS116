5d72e142073da15c28e090a86bcfd93d36c3b6b4
Manya06-ctrl <tarajoshi8101972@gmail.com>
2026-06-21 20:13:31 +0530

    Day_16 uploaded

---
+++ Day_16/61.c
@@ -0,0 +1,25 @@
+// find missing number in array
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, sum = 0, total, missing;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        sum = sum + a[i];
+    }
+
+    total = (n + 1) * (n + 2) / 2;
+
+    missing = total - sum;
+
+    printf("Missing number = %d", missing);
+
+    return 0;
+}
